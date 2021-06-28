1de74bc31aead8c0236c7234393f708976d261c0
lamhung <lamhungypl@gmail.com>
2021-06-28 13:15:41 +0700

    feat(setup): setup apollo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      // installSubscriptionHandlers: true,
+      autoSchemaFile: true,
+    }),
+
+    RecipesModule,
+  ],
 })
 export class AppModule {}
